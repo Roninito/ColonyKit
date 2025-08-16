851915dab48c145e19d0ae423f87eb1272ab5792
Leerie Simpson <leerie.a.simpson@gmail.com>
2025-08-16 15:55:44 -0400

    Testing RCP and dependencies.

---
+++ Sources/ColonyKit/PlanetoidComponent.swift
@@ -0,0 +1,13 @@
+//
+//  Untitled.swift
+//  ColonyKit
+//
+//  Created by Leerie Simpson on 8/16/25.
+//
+
+import RealityKit
+
+public struct PlanetoidComponent: Component {
+    public var name: String = "Untitled"
+    public var population: Int = 0
+}
